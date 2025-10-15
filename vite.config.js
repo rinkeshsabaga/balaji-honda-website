ea3bf255c507381c63bcbc0f1c7a187239b056e8
rinkeshsabaga <rinkesh@sabaga.in>
2025-10-15 15:55:09 +0530

    Initial commit - Balaji Honda website

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/balaji-honda-website/',
-  server: {
-    port: 5173,
-    open: true
-  },
+  base: '/',
   build: {
-    outDir: 'dist',
-    sourcemap: true
+    outDir: 'dist'
   }
 })
